829379c0af0d50a8a1752efcebf766c0e41b0051
Pulak Tandon <pulaktandon2000@gmail.com>
2022-09-06 01:14:14 +0530

    Join

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -36,7 +36,7 @@ export const listProducts = (keyword = '') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get(`/api/products${keyword}`)
+        const { data } = await axios.get(`https://gadgetzbackend.herokuapp.com//api/products${keyword}`)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
